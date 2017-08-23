c6ad5c30e948c297d49c344422ea2724a7ae93e0
devbywater <egaraudy@gmail.com>
2017-08-23 15:39:15 -0600

    clean out unnecessary project folders

--- ExpressGraphServer/customer_data.js
+++
@@ -1,6 +0,0 @@
-
-export default customers = [
-    {id:'1', name:'John Doe', email:'jdoe@gmail.com', age:36},
-    {id:'2', name:'Pat Doe', email:'pat@gmail.com', age:19},
-    {id:'3', name:'Steve Smith', email:'ssmith@gmail.com', age:23}
-]
--- Users-graph/server.js
+++ Users-graph/server.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const expressGrapQL = require('express-graphql')
-const PORT = process.env.PORT || 3030
+const express = require('express');
+const expressGrapQL = require('express-graphql');
+
+const schema = require('./schema/schema');
+
+const PORT = process.env.PORT || 3030;
 
 
-const app = express()
+const app = express();
 
 app.use('/graphql', expressGrapQL({
+    schema,
     graphiql: true
-}))
+}));
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
-})
+});
